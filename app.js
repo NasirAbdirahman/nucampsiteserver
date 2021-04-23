d48dd0a1ed13b740d94e2554dd9b77c474e5e77f
AbdulNasir Abdirahman <nasir.a.abdirahman@outlook.com>
2021-04-23 11:02:04 -0700

    Set up basic Authentication

--- app.js
+++ app.js
@@ -42,6 +42,34 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+//Authentication Added here-b/c this is first of middleware Fn that send things back to client
+//So users must authenticate before they access data from the server
+function auth(req, res, next) {
+  console.log(req.headers);
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+      const err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');//Lets client know, server is req. authentication
+      err.status = 401;
+      return next(err);
+  }
+
+  const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+  const user = auth[0];
+  const pass = auth[1];
+  if (user === 'admin' && pass === 'password') {
+      return next(); // authorized
+  } else {
+      const err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');      
+      err.status = 401;
+      return next(err);
+  }
+}
+
+app.use(auth);
+
 app.use(express.static(path.join(__dirname, 'public')));//Built in middleware that serves static files
 
 //App using the Routers
